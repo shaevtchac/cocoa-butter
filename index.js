bf245ef05c931f4c6e07f6dce28f522eaca21a83
shaevtchac <shaevtchac@gmail.com>
2023-06-14 10:39:19 +0200

    animations transitions

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 // _______________________________________________________________ animations _______________________________________________________________
 
-// const animatedElements = document.querySelectorAll('[data-animation]');
+const animatedElements = document.querySelectorAll('[data-animation]');
 
-// const animationObserver = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     const {
-//       animation,
-//       duration = '500ms',
-//       'timing-function': timingFunction = 'ease-in',
-//       delay = '0ms',
-//       'iteration-count': iterationCount = '1',
-//       direction = 'normal',
-//       'fill-mode': fillMode = 'backwards',
-//     } = entry.target.dataset;
-//     if (entry.isIntersecting) {
-//       entry.target.style.animation = `${animation} ${duration} ${timingFunction} ${delay} ${iterationCount} ${direction} ${fillMode}`;
-//     } else {
-//       entry.target.style.animation = 'none';
-//     }
-//   });
-// });
+const animationObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const {
+      animation,
+      duration = '500ms',
+      'timing-function': timingFunction = 'ease-in',
+      delay = '500ms',
+      'iteration-count': iterationCount = '1',
+      direction = 'normal',
+      'fill-mode': fillMode = 'backwards',
+    } = entry.target.dataset;
+    if (entry.isIntersecting) {
+      entry.target.style.animation = `${animation} ${duration} ${timingFunction} ${delay} ${iterationCount} ${direction} ${fillMode}`;
+    } else {
+      entry.target.style.animation = 'none';
+    }
+  });
+});
 
-// animatedElements.forEach((element) => {
-//   animationObserver.observe(element);
-// });
+animatedElements.forEach((element) => {
+  animationObserver.observe(element);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,26 @@ module.exports = {
         lato: ['Lato', 'sans-serif'],
         courgette: ['Courgette', 'cursive'],
       },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-6deg) scaleX(-1)' },
+          '50%': { transform: 'rotate(6deg) scaleX(-1)' },
+        },
+        bounce1: {
+          '0%, 100%': {
+            transform: 'translateY(-25%) scale(1,1)',
+            'animation-timing-function': 'cubic-bezier(0.8,0,1,1)',
+          },
+          '50%': {
+            transform: 'scale(1.1,.9)',
+            'animation-timing-function': 'cubic-bezier(0,0,0.2,1)',
+          },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 250ms linear ',
+        bounce1: 'bounce1 1s cubic-bezier(0.280, 0.840, 0.420, 1) infinite',
+      },
     },
   },
 
